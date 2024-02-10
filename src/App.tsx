6018dd783bf45be6546ab2e21e93d95075d4e4b6
Neil Irungu <ruth.irungu118@gmail.com>
2024-02-10 18:06:35 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript with Neil Irungu.
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
