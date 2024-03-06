0a7d73370617f5a2afb53cb23c00729fd224c961
Neil Irungu <ruth.irungu118@gmail.com>
2024-03-06 13:45:27 -0500

    Finished fixing ChooseTeam

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -14,18 +14,19 @@ export function ChooseTeam(): JSX.Element {
     const [allOptions, setAllOptions] = useState<string[]>(PEOPLE);
     const [team, setTeam] = useState<string[]>([]);
 
-    function chooseMember() {
-        /*
+    function chooseMember(newMember: string) {
+        let newTeam = [...team];
         if (!team.includes(newMember)) {
-            team.push(newMember);
+            newTeam = [...newTeam, newMember];
         }
-        */
+        setTeam(newTeam);
     }
 
     function clearTeam() {
         /*
         team = [];
         */
+        setTeam([]);
     }
 
     return (
@@ -36,7 +37,10 @@ export function ChooseTeam(): JSX.Element {
                     {allOptions.map((option: string) => (
                         <div key={option} style={{ marginBottom: "4px" }}>
                             Add{" "}
-                            <Button onClick={chooseMember} size="sm">
+                            <Button
+                                onClick={() => chooseMember(option)}
+                                size="sm"
+                            >
                                 {option}
                             </Button>
                         </div>
