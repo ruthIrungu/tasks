ad75c7aab735f07bd5e7b4641b260da3101005ee
Neil Irungu <ruth.irungu118@gmail.com>
2024-03-10 18:53:16 -0400

    Finished all form components

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,37 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [currOption, setCurrOption] = useState<string>(options[0]);
+    const [correct, setCorrect] = useState<string>("❌");
+
+    function updateOption(event: React.ChangeEvent<HTMLSelectElement>) {
+        setCurrOption(event.target.value);
+        if (currOption === expectedAnswer) {
+            setCorrect("✔️");
+        } else {
+            setCorrect("❌");
+        }
+    }
+
     return (
         <div>
-            <h3>Multiple Choice Question</h3>
+            <div>
+                <h3>Multiple Choice Question</h3>
+            </div>
+            <div>
+                <Form.Group controlId="multipleChoice">
+                    <Form.Label>Which option?</Form.Label>
+                    <Form.Select value={currOption} onChange={updateOption}>
+                        {options.map((option: string) => (
+                            <option key={option} value={option}>
+                                {" "}
+                                option{" "}
+                            </option>
+                        ))}
+                    </Form.Select>
+                </Form.Group>
+                <div>{correct}</div>
+            </div>
         </div>
     );
 }
